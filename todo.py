330132cfb12959227e5edb458beb83d7e557c4ec
Mark Striemer <mstriemer@gmail.com>
2012-10-16 21:40:31 -0500

    add edit

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -12,7 +13,11 @@ class TodoRepository(object):
         todo.id = len(self.todos) + 1
         self.todos.append(todo)
 
+    def update(self, todo):
+        pass
+
     def find(self, id):
+        id = int(id)
         if 0 <= id - 1 < len(self.todos):
             todo = self.todos[id - 1]
         else:
@@ -38,29 +43,21 @@ todo_repository = TodoRepository()
 todo_repository.save(Todo("Learn Flask"))
 todo_repository.save(Todo("Make an API"))
 
+def find_todo(fn):
+    @wraps(fn)
+    def _inner(id):
+        todo = todo_repository.find(id)
+        if todo is None:
+            return "Not Found", 404
+        else:
+            return fn(id, todo)
+    return _inner
+
+
 @app.route('/', methods=['GET'])
 def index():
     todos = [todo.as_dict() for todo in todo_repository.all()]
     return jsonify(todos=todos)
-
-@app.route('/<id>/', methods=['GET'])
-def show(id):
-    id = int(id)
-    todo = todo_repository.find(id)
-    if todo is None:
-        return "Not Found", 404
-    else:
-        return jsonify(todo.as_dict())
-
-@app.route('/<id>/', methods=['DELETE'])
-def destroy(id):
-    id = int(id)
-    todo = todo_repository.find(id)
-    if todo is None:
-        return "Not Found", 404
-    else:
-        todo_repository.remove(todo)
-        return '', 204
 
 @app.route('/', methods=['POST'])
 def create():
@@ -71,6 +68,28 @@ def create():
         todo = Todo(name)
         todo_repository.save(todo)
         return jsonify(todo.as_dict()), 201
+
+@app.route('/<id>/', methods=['GET'])
+@find_todo
+def show(id, todo):
+    return jsonify(todo.as_dict())
+
+@app.route('/<id>/', methods=['DELETE'])
+@find_todo
+def destroy(id, todo):
+    todo_repository.remove(todo)
+    return '', 204
+
+@app.route('/<id>/', methods=['PUT'])
+@find_todo
+def edit(id, todo):
+    name = request.form.get('name', '')
+    if not name:
+        return "name is required", 322
+    else:
+        todo.name = name
+        todo_repository.update(todo)
+        return jsonify(todo.as_dict())
 
 if __name__ == '__main__':
     app.run(debug=True)
